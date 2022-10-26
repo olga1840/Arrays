6652ece9dcf8e47c7847cc8d107a455794b2f76c
Rodch_ot15@top-academy.ru <Rodch_ot15@top-academy.ru>
2022-10-26 22:30:53 +0300

    Random done

--- Random/Source.cpp
+++ Random/Source.cpp
@@ -4,16 +4,24 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int n = 5;
+
+	int num_1; int num_2;
+	cout << "Введите минимальное число диапазона для вывода чисел: "; cin >> num_1;
+	cout << "Введите максимальное число диапазона для вывода числе: "; cin >> num_2;
+
+	const int n = 10;
 	int arr[n];
+	
 	for (int i = 0; i < n; i++)
 	{
-		arr[i] = rand()%100;
+		arr[i] = rand() % (num_2-num_1+1)+num_1;
 	}
+
 	for (int i = 0; i < n; i++)
 
 	{
 		cout << arr[i] << "\t";
 	}
+	
 	cout << endl;
 }
